c2affacc5c9ad4f3517fb31a5ab001199cc61407
Arnob Chandra Das <arnobdas.dev@gmail.com>
2026-07-27 01:47:20 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     // Important for GitHub Pages
-    base: '/aether-dragon-motion/',
+    base: '/aether_dragon_motion/',
 
     plugins: [react(), tailwindcss()],
 
